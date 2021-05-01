0e19bb7b687fe1e3faaa7fef50fe44631fc76a87
Naxoht <73888159+Naxoht@users.noreply.github.com>
2021-05-01 21:06:07 +0200

    Update paquetealtresgestions.sql

--- paquetealtresgestions.sql
+++ paquetealtresgestions.sql
@@ -66,6 +66,26 @@ END;
 
 
 --Consultas de los usuarios
+procedure consulta_usuari_cognoms(cognom varchar2)
+as
+
+cursor usuari is
+select * from users
+where lastname = cognom;
+cont_usuari_cog number;
+BEGIN
+select count(*) into cont_usuari_cog from users
+where lastname = cognom;
+if cont_usuari_cog != 0 then
+    dbms_output.put_line(rpad('CODI_USUARI',15)||rpad('USUARI',17)||rpad('NOM',17)||rpad('COGNOM',17)||'CONTRASENYA');
+    for i in usuari loop
+        dbms_output.put_line(rpad(i.user_code,15)||rpad(i.name,17)||rpad(i.firsname,17)||rpad(i.lastname,17)||i.password);
+    end loop;
+else
+    dbms_output.put_line('No existeix un usuari amb aquest cognom');
+end if;
+END;
+
 procedure consulta_usuaris
 as
 cursor usuaris is 
